9c1901ec7ea72a6baa6201ee30184877de655a83
EatingPika <pierre.finantz@gmail.com>
2017-05-11 23:50:01 +0200

    Update index.php
    add support for integration in subfolder (for the image in the select lang area)

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 -->
 <?php
     $path='langManagementSystem/';
+    $toRoot="";
     require_once "$path"."lang_responsive.php";
 ?>
 <html>
